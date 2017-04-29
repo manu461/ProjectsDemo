60aa892025843cc01c4a316d167af0a963db6046
manu461 <rastogimanu461@gmail.com>
2017-04-30 05:09:22 +0530

    -
    -

--- AndroidProjects/IncompleteCrimeApp/BNRTwoCriminalIntent/app/src/main/java/com/example/stalker/bnrtwocriminalintent/CrimeListActivity.java
+++ AndroidProjects/IncompleteCrimeApp/BNRTwoCriminalIntent/app/src/main/java/com/example/stalker/bnrtwocriminalintent/CrimeListActivity.java
@@ -9,6 +9,7 @@ import android.support.v4.app.Fragment;
 public class CrimeListActivity extends SingleFragmentActivity {
     @Override
     protected Fragment createFragment() {
+        //....
         return new CrimeListFragment();
     }
 }
