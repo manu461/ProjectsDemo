c9e7bbedbf17541521e4b219357040ffd61f289c
manu461 <rastogimanu461@gmail.com>
2018-10-28 14:55:27 +0530

    Added view pager
    view pager linked with step view... all working fine

--- AndroidProjects/MasteringBlockchain/app/src/main/java/e/dekod/masteringblockchain/TopicFragment.java
+++ AndroidProjects/MasteringBlockchain/app/src/main/java/e/dekod/masteringblockchain/TopicFragment.java
@@ -1,7 +1,46 @@
 package e.dekod.masteringblockchain;
 
 import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
-public class TopicFragment extends Fragment {
+import java.util.UUID;
 
+import e.dekod.masteringblockchain.Beans.Topic;
+
+public class TopicFragment extends android.support.v4.app.Fragment {
+
+    private static final String KEY = "e.dekod.masteringblockchain.TopicFragment";
+
+    private Topic topic;
+    private TextView topicSerialTextView;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        topic = (Topic) getArguments().getSerializable(KEY);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_topic,container,false);
+        topicSerialTextView = view.findViewById(R.id.topicSerial_textView);
+
+        topicSerialTextView.setText(topic.getTopicSerialId()+"");
+
+        return view;
+    }
+
+    public static TopicFragment getInstance(Topic topic) {
+        Bundle args = new Bundle();
+        args.putSerializable(KEY,topic);
+        TopicFragment fragment = new TopicFragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
 }
--- AndroidProjects/MasteringBlockchain/app/src/main/java/e/dekod/masteringblockchain/TopicsActivity.java
+++ AndroidProjects/MasteringBlockchain/app/src/main/java/e/dekod/masteringblockchain/TopicsActivity.java
@@ -2,10 +2,17 @@ package e.dekod.masteringblockchain;
 
 import android.content.Context;
 import android.content.Intent;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.content.ContextCompat;
 import android.support.v4.content.res.ResourcesCompat;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Html;
+import android.widget.TextView;
 
 import com.shuhart.stepview.StepView;
 
@@ -16,10 +23,12 @@ import e.dekod.masteringblockchain.Beans.Chapter;
 import e.dekod.masteringblockchain.Beans.Topic;
 import e.dekod.masteringblockchain.Beans.Unit;
 
-public class TopicsActivity extends AppCompatActivity {
+public class TopicsActivity extends FragmentActivity {
 
     private static final String KEY = "e.dekod.masteringblockchain.TopicsActivity";
     private Unit unit;
+    private ViewPager topicViewPager;
+    private TextView unitTitleTextView;
     private ArrayList<Topic> allTopicsList;
     private StepView stepView;
 
@@ -32,23 +41,56 @@ public class TopicsActivity extends AppCompatActivity {
 
         allTopicsList = (ArrayList<Topic>) unit.getTopicsOfUnit();
 
+        topicViewPager = findViewById(R.id.topic_viewPager);
         stepView = findViewById(R.id.step_view);
-        stepView.getState()
-                .selectedTextColor(ContextCompat.getColor(this, R.color.colorAccent))
-                .animationType(StepView.ANIMATION_CIRCLE)
-                .selectedCircleColor(ContextCompat.getColor(this, R.color.colorAccent))
+        unitTitleTextView = findViewById(R.id.topics_unit_title_textView);
 
-                .selectedStepNumberColor(ContextCompat.getColor(this, R.color.colorPrimary))
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        topicViewPager.setAdapter(new FragmentPagerAdapter(fragmentManager) {
+            @Override
+            public Fragment getItem(int position) {
+                return TopicFragment.getInstance(allTopicsList.get(position));
+            }
+
+            @Override
+            public int getCount() {
+                return allTopicsList.size();
+            }
+
+        });
+        topicViewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                unitTitleTextView.setText(Html.fromHtml("<b>hello</b>"));
+                stepView.go(position,true);
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+
+        stepView.getState()
+                .selectedTextColor(ContextCompat.getColor(this, android.R.color.holo_green_light))
+                .animationType(StepView.ANIMATION_CIRCLE)
+                .selectedCircleColor(ContextCompat.getColor(this, android.R.color.holo_green_light))
+                .selectedStepNumberColor(ContextCompat.getColor(this, android.R.color.white))
                 // You should specify only stepsNumber or steps array of strings.
                 // In case you specify both steps array is chosen.
-                .steps(new ArrayList<String>() {{
-                    add("First step");
-                    add("Second step");
-                    add("Third step");
-                }})
+//                .steps(new ArrayList<String>() {{
+//                    add("First step");
+//                    add("Second step");
+//                    add("Third step");
+//                }})
                 // You should specify only steps number or steps array of strings.
                 // In case you specify both steps array is chosen.
-                .stepsNumber(4)
+                .stepsNumber(allTopicsList.size())
                 .animationDuration(getResources().getInteger(android.R.integer.config_shortAnimTime))
 
 
